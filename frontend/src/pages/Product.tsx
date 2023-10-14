ca46f5aa56f7533c8d2291b1bd4df8097cc91889
pye <142072887+PyeThunpisit@users.noreply.github.com>
2023-10-14 12:56:20 +0700

    Revert "nothing" This reverts commit 8135de6c4308cc9636b463d26ef12f642bcd77b

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import product1 from '../assets/laptop.webp';
 import "../style/productStyle.css"
-import Sitebar from './Sitebar';
-
+import { ProductInterface } from '../interfaces/IProduct';
+import {GetProduct} from '../services/https/productIndex';
 function Product() {
+    const [products, setProdcuts] = useState<ProductInterface[]>([]);
+    const getProduct = async () => {
+      let res = await GetProduct();
+      if (res) {
+        setProdcuts(res);
+      }
+      };
+      useEffect(() => {
+        getProduct();
+        }, []);
   return (
-      
       <div className="container_product" >
         <div className="product_con">
           <div className="product_item">
             <div className="product_img">
-              <img src={product1}  />
+              <img src ={product1}/>
             </div>  
-              <div className='button'> 
+              <div className='button' > 
                 ซื้อเลย
               </div>
-              <div className='product_name'>
+              <div className='product_name' >
                 Laptop VICTUS Pro max
               </div>  
                 <div className="product_detail">
@@ -28,7 +37,7 @@ function Product() {
           </div>           
         </div>      
       </div>
+     
   );
 }
-
 export default Product;
